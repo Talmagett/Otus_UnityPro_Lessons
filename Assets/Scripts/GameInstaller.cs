be6e7f247a304891ed84e1a20eb34f9f6406a89f
Talmagett <talmagett@gmail.com>
2024-01-28 16:19:21 +0600

    added Zenject, removed entry point

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,16 @@
+using GameEngine;
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    [SerializeField] private UnitManager unitManager;
+
+    [SerializeField] private ResourceService resourceService;
+
+    public override void InstallBindings()
+    {
+        unitManager.SetupUnits(FindObjectsOfType<Unit>());
+        resourceService.SetResources(FindObjectsOfType<Resource>());
+    }
+}
