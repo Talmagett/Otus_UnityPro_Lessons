84e1c26b2686e38b1975b8a35de46bfad9b35496
Talmagett <talmagett@gmail.com>
2024-02-13 10:44:12 +0600

    added items

--- Assets/Scripts/Character/DebugCharacter.cs
+++ Assets/Scripts/Character/DebugCharacter.cs
@@ -1,6 +1,7 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Zenject;
+using System.Reflection;
 
 namespace Sample
 {
@@ -17,8 +18,20 @@ namespace Sample
         [Button]
         public void GetStats()
         {
-            Debug.ClearDeveloperConsole();
-            foreach (var (key, value) in _character.GetStats()) print($"{key} : {value}");
+            ClearLog();
+            string printer="";
+            foreach (var (key, value) in _character.GetStats()) 
+                printer+=($"{key} : {value} \n");
+            print(printer);
+        }
+        
+        //этот класс не должен находиться здесь, чисто ради дебага оставил, ибо лень )
+        private void ClearLog()
+        {
+            var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+            var type = assembly.GetType("UnityEditor.LogEntries");
+            var method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
         }
     }
 }
